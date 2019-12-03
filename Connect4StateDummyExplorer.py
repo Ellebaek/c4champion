01bcb5e916e1e986907a90cddc1e531020f12d32
Ellebaek <t.ellebaek@gmail.com>
2019-12-03 12:15:48 +0000

    Added simple game print

---
+++ Connect4StateDummyExplorer.py
@@ -0,0 +1,28 @@
+import ast
+import numpy as np
+
+class GamePrinter():
+    def __init__(self, state_list_file):
+        self.board_sequence = []
+        f = open(state_list_file, "r")
+        if f.mode == "r":
+            lines = f.readlines()
+            for l in lines:
+                self.board_sequence.append(ast.literal_eval(l[:-1]))
+        f.close()
+        self.current_state_id = 0
+
+    def print_game(self):
+        for i in range(len(self.board_sequence)):
+            print(i)
+            print(np.array(self.board_sequence[i]))
+
+# main method
+def main():
+    gp = GamePrinter(state_list_file="c4games/duel_15x1001_game1.txt")
+    gp.print_game()
+    gp = GamePrinter(state_list_file="c4games/duel_15x1001_game2.txt")
+    gp.print_game()
+
+if __name__ == "__main__":
+    main()
