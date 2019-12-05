e32ee1164623caf569ea4d87bda7b03f3d7f7022
Ellebaek <t.ellebaek@gmail.com>
2019-12-05 21:41:02 +0000

    Updated file path

--- Connect4StateDummyExplorer.py
+++ Connect4StateDummyExplorer.py
@@ -19,9 +19,9 @@ class GamePrinter():
 
 # main method
 def main():
-    gp = GamePrinter(state_list_file="c4games/duel_15x501_game1.txt")
+    gp = GamePrinter(state_list_file="c4games/duel_100x500_game1.txt")
     gp.print_game()
-    gp = GamePrinter(state_list_file="c4games/duel_15x501_game2.txt")
+    gp = GamePrinter(state_list_file="c4games/duel_100x500_game2.txt")
     gp.print_game()
 
 if __name__ == "__main__":
