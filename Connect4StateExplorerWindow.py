c5d9b581eb49b2c71791692c52ec3916ba2c377b
Ellebaek <t.ellebaek@gmail.com>
2019-12-21 18:04:22 +0100

    updated file path

--- Connect4StateExplorerWindow.py
+++ Connect4StateExplorerWindow.py
@@ -50,7 +50,7 @@ class Connect4StateExplorerWindow(Connect4GameWindow):
 
 # main method
 def main():
-    Connect4StateExplorerWindow(state_list_file="c4games/duel_15x1001_game1.txt")
+    Connect4StateExplorerWindow(state_list_file="c4games/duel_55x100_game1.txt")
     arcade.run()
 
 
